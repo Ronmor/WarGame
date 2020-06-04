f2a62594156c9418b436335f4210f6628f15f4f3
Ron Mor <ronmor35@gmail.com>
2020-06-04 10:53:36 +0300

    Update Demo.cpp

--- Demo.cpp
+++ Demo.cpp
@@ -1,11 +1,11 @@
 
 #include <iostream>
-//#include "DemoGame.hpp"  
+#include "DemoGame.hpp"  
 
-//using namespace std;
+using namespace std;
 using namespace WarGame;
 int main() {
-	//WarGame::DemoGame demoGame;  // Initialize a game on a 8-by-8 board
-	//cout << "The winner is:\n " << demoGame.play() << endl;
+	WarGame::DemoGame demoGame;  // Initialize a game on a 8-by-8 board
+	cout << "The winner is:\n " << demoGame.play() << endl;
     return 0;
 }
